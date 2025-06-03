470611937a76235e310bcc2c9504ed139a3d51fb
Andres Moreno <anfelmoreno@gmail.com>
2025-06-02 20:12:39 -0500

    actualizando el dotenv

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 // server/server.js
+require('dotenv').config();
+const dotenv = require('dotenv');
 const bcrypt = require("bcryptjs");
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
 
 dotenv.config();
